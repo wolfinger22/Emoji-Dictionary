b2af3cd74feeb43f2657e757562c439792a03996
Wolf Wittmund <>
2018-12-27 17:13:50 +0100

    First Steps

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableviewdataSource, UITableViewdelegate {
 
+    @IBOutlet weak var TableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.!!
+        
+        TableView.dataSource = self
+        TableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
